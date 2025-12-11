4014cd7fc04e145465fb7011d6173179cb011fa6
nanditaprabhu-oss <nandita.prabhu@visma.com>
2025-12-11 18:14:47 +0530

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,16 +22,11 @@ export default defineConfig({
   retries: 2, // optional, helps reduce flakiness
   workers: 2,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
- reporter: 'html'
+ reporter: 'html',
 
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like await page.goto(''). */
-    // baseURL: 'http://localhost:3000',
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-    video: 'on',
     headless: true
   },
 
