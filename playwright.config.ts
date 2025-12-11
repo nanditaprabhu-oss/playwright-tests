a30c2cea44c16dcbf77c9ed5f9a0095ad600b1cd
nanditaprabhu-oss <nandita.prabhu@visma.com>
2025-12-11 16:11:24 +0530

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: 2, // optional, helps reduce flakiness
-  workers: 4,
+  workers: 2,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
